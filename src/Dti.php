daf3f05d59684372454ee962a71ff4152021f549
Brett Ramin <bramin@C02W37XRHTD6.local>
2019-05-28 09:32:59 -0700

    Add docblocks for DTI

--- src/Dti.php
+++ src/Dti.php
@@ -6,9 +6,25 @@ class Dti
 {
     const PERCENT_PRECISION = 3;
 
+    /**
+     * Array with all borrower data for a loan
+     *
+     * @var array $borrowers
+     */
     protected $borrowers;
+
+    /**
+     * Array with all coborrower data for a loan
+     *
+     * @var array $coborrowers
+     */
     protected $coborrowers;
 
+    /**
+     * Keys for items to include when calculating the total monthly payment for the loan.
+     *
+     * @var array $liabilityItems
+     */
     protected $liabilityItems = [
         'first_mortgage',
         'other_financing',
@@ -20,6 +36,12 @@ class Dti
         'utilities',
     ];
 
+    /**
+     * Keys for items to include when calculating each borrowers total monthly income. Values are
+     * the name of the key used to determine whether or not the item is to be used in the ratio.
+     *
+     * @var array $incomeItems
+     */
     protected $incomeItems = [
         'base_income'        => 'baseInRatio',
         'overtime'           => 'overtimeInRatio',
@@ -39,6 +61,12 @@ class Dti
         $this->coborrowers = $appData['coborrowers'];
     }
 
+    /**
+     * DTI Front represents the Debt To Income ration for a specific
+     * loan and does not consider debt related to other loans.
+     *
+     * @return string
+     */
     public function getDtiFront()
     {
         $mortgageLiability = $this->getMortgageLiability();
@@ -47,6 +75,12 @@ class Dti
         return self::convertToPercent($mortgageLiability / $totalIncome);
     }
 
+    /**
+     * DTI Back represents the total Debt To Income ration for a specific
+     * loan including debt related to other loans.
+     *
+     * @return string
+     */
     public function getDtiBack()
     {
         $mortgageLiability = $this->getMortgageLiability();
@@ -56,6 +90,12 @@ class Dti
         return self::convertToPercent(($mortgageLiability + $otherLiability) / $totalIncome);
     }
 
+    /**
+     * The total monthly payment for the mortgage being considered.  This includes bills such
+     * as HOA dues, interest, tax insurance and utilities on the property.
+     *
+     * @return float
+     */
     protected function getMortgageLiability()
     {
         $mortgageLiability = 0;
@@ -67,11 +107,23 @@ class Dti
         return $mortgageLiability;
     }
 
+    /**
+     * The sum of monthly liabilities for all borrowers on the application.
+     *
+     * @return float
+     */
     protected function getOtherLiability()
     {
         return $this->getBorrowersLiabilities($this->borrowers) + $this->getBorrowersLiabilities($this->coborrowers);
     }
 
+    /**
+     * The sum of borrowers monthly payments for all items not related to the proposed mortgage.
+     * This includes debts such as alimony, child care, child support, collections and payments on unrelated loans.
+     *
+     * @param array $borrowers
+     * @return float
+     */
     protected function getBorrowersLiabilities($borrowers)
     {
         $totalLiabilities = 0;
@@ -93,11 +145,23 @@ class Dti
         return $totalLiabilities;
     }
 
+    /**
+     * The sum of monthly income for all borrowers on the application.
+     *
+     * @return float
+     */
     protected function getTotalIncome()
     {
         return $this->getBorrowersIncome($this->borrowers) + $this->getBorrowersIncome($this->coborrowers);
     }
 
+    /**
+     * The sum of borrowers monthly income.  This includes income such as alimony, child care,
+     * bonuses, commissions and SSI/disability.  This does NOT include rental incomes.
+     *
+     * @param array $borrowers
+     * @return float
+     */
     protected function getBorrowersIncome($borrowers)
     {
         $totalIncome = 0;
@@ -121,6 +185,12 @@ class Dti
         return $totalIncome;
     }
 
+    /**
+     * Convert float (ratio) to percentage string.
+     *
+     * @param float $value
+     * @return float
+     */
     protected static function convertToPercent($value)
     {
         return number_format(round($value * 100, self::PERCENT_PRECISION), self::PERCENT_PRECISION) . '%';
