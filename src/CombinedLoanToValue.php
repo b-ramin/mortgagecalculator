03f43d7ec6b2bc4bf5cef084fac0ef8067c2494f
Brett Ramin <bramin@C02W37XRHTD6.local>
2019-05-28 11:53:08 -0700

    Add docblocks for LTV and cLTV

--- src/CombinedLoanToValue.php
+++ src/CombinedLoanToValue.php
@@ -4,15 +4,23 @@ namespace MortgageCalculator;
 
 class CombinedLoanToValue extends LoanToValue
 {
+    /**
+     * Determine the total loan amount to be used for the calculation. For cltv, the loan amount is
+     * totalLoanAmount plus subFinanceAmount.  totalLoanAmount includes financed closing costs.
+     *
+     * @return float
+     */
     protected function getLoanAmount()
     {
-        if (empty($this->subFinanceAmount)) {
-            return parent::getLoanAmount();
-        }
-
         return $this->totalLoanAmount + $this->subFinanceAmount;
     }
 
+    /**
+     * Determine the property value to be used for the calculation. For cltv, if subFinanceAmount is not
+     * empty, the value falls back from most accurate to least accurate depending on availability.
+     *
+     * @return float
+     */
     protected function getValue()
     {
         if (empty($this->subFinanceAmount)) {
@@ -23,6 +31,12 @@ class CombinedLoanToValue extends LoanToValue
 
     }
 
+    /**
+     * Determine the first non-empty value passed in an array.
+     *
+     * @param $array
+     * @return float|null
+     */
     private static function firstNonEmpty($array)
     {
         foreach ($array as $value) {
--- src/LoanToValue.php
+++ src/LoanToValue.php
@@ -27,6 +27,11 @@ class LoanToValue
         $this->subFinanceAmount   = $subFinanceAmount;
     }
 
+    /**
+     * Divide loan amount by value and return result as formatted string.
+     *
+     * @return string
+     */
     public function calculate()
     {
         $loanAmount = $this->getLoanAmount();
@@ -35,16 +40,33 @@ class LoanToValue
         return self::convertToPercent($loanAmount / $value);
     }
 
+    /**
+     * Determine the total loan amount to be used for the calculation.
+     * originalLoanAmount does not include financed closing costs.
+     *
+     * @return float
+     */
     protected function getLoanAmount()
     {
         return $this->originalLoanAmount;
     }
 
+    /**
+     * Determine the property value to be used for the calculation.
+     *
+     * @return float
+     */
     protected function getValue()
     {
         return $this->estimatedValue;
     }
 
+    /**
+     * Convert float (ratio) to percentage string.
+     *
+     * @param float $value
+     * @return float
+     */
     protected static function convertToPercent($value)
     {
         return number_format(round($value * 100, self::PERCENT_PRECISION), self::PERCENT_PRECISION);
